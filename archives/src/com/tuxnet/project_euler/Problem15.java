6e069419137366c228bc39ac8c44360069ab2a35
mrl5 <jakuboo+github@gmail.com>
2017-09-16 22:49:14 +0200

    solution: 407575348

--- archives/src/com/tuxnet/project_euler/Problem15.java
+++ archives/src/com/tuxnet/project_euler/Problem15.java
@@ -11,5 +11,24 @@ How many such routes are there through a 20×20 grid?
  */
 
 public class Problem15 {
-    //TODO: do some reaserch about Combinatorics/permutations?
+    //TODO: do some research about Combinatorics/permutations?
+    //nice solution: http://code.jasonbhill.com/python/project-euler-problem-15/
+    public static void solve() {
+        int grid = 20;
+        int rows = grid;
+        int columns = grid;
+        rows++;
+        columns++;
+        Integer[][] a = new Integer[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (i == 0 || j == 0) a[i][j] = 1;
+                else a[i][j] = a[i - 1][j] + a[i][j - 1];
+            }
+        }
+        rows--;
+        columns--;
+        System.out.println(a[rows][columns]);
+    }
 }
