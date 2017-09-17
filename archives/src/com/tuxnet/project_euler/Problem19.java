cfeb4d206b0a18ae43f987403c068658a36a647f
mrl5 <jakuboo+github@gmail.com>
2017-09-17 13:27:12 +0200

    solution: 171

--- archives/src/com/tuxnet/project_euler/Problem19.java
+++ archives/src/com/tuxnet/project_euler/Problem19.java
@@ -19,5 +19,66 @@ How many Sundays fell on the first of the month during the twentieth century (1 
      */
 
 public class Problem19 {
-    //TODO: some nice algoritms
+    private static int days = 30;
+    private static int sundays = 0;
+
+    public static void solve() {
+        int year = 1900;
+        int month = 1;
+        int day = 1;
+        int dc = 0;
+        boolean flag = false;
+
+        while (year < 2000) {
+            thirty(month, year);
+
+            if (day == days) {
+                if (month == 12) {
+                    year++;
+                    flag = true;
+                    month = 0;
+                }
+                month++;
+                day = 0;
+            }
+            day++;
+            dc++;
+
+            switch (dc) {
+                case 7:
+                    if (day == 1 && flag)
+                        sundays++;
+                    dc = 0;
+                    break;
+            }
+        }
+        System.out.println("During the twentieth century there was " + sundays + " sundays on the first of the month");
+    }
+
+    private static void thirty(int month, int year) {
+        switch (month) {
+            case 2:
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                    days = 29;
+                else
+                    days = 28;
+                break;
+            case 4:
+                days = 30;
+                break;
+            case 6:
+                days = 30;
+                break;
+            case 9:
+                days = 30;
+                break;
+            case 11:
+                days = 30;
+                break;
+            default:
+                days = 31;
+                break;
+        }
+    }
+
 }
